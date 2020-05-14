dbfe0e273dc8179ac5ac703b60db30002b29d318
Erick Pena <erick1439@live.com>
2020-05-13 22:20:05 -0400

    state for register component added

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,35 +1,85 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import axios from 'axios';
 
 export default class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: ""
+        };
+
+        this.formChange = this.formChange.bind(this);
+    }
+
+    formChange = event => {
+
+        const target = event.target;
+        const value = target.name === 'isGoing' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+
+        // or 
+        // this.setState({ [e.target.name]: e.target.value});
+    }
+
+    submitHandler = event => {
+         event.preventDefault();
+        console.log(this.state);
+        axios.post('http://localhost:5000/login', this.state)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/')
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.submitHandler}>
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input type="text" className="form-control" placeholder="First name" onChange={this.formChange} name="firstName" />
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input type="text" className="form-control" placeholder="Last name" onChange={this.formChange} name="lastName" />
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control" placeholder="Enter email" onChange={this.formChange} name="email" />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" className="form-control" placeholder="Enter password" onChange={this.formChange} name="password" />
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
                 <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
+                    Already registered <a href="/">sign in?</a>
                 </p>
             </form>
         );
