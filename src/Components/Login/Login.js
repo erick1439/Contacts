960c42e9e62026d6dc04a72515dcd279c02fa88e
Erick Peña <erickscomputer@Ericks-MBP.mshome.net>
2020-05-18 15:33:29 -0400

    home page added to routing

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './Login.css';
 import axios from 'axios';
 
+
+import Home from '../Home/Home'
+
 export default class Login extends Component {
     constructor () {
         super();
@@ -39,31 +42,35 @@ export default class Login extends Component {
 
     render() {
         return (
-            <form onSubmit={this.submitHandler}> 
-                <h3>Sign In</h3>
+            <div className="auth-wrapper">
+                <div className="auth-inner">
+                    <form onSubmit={this.submitHandler}> 
+                        <h3>Sign In</h3>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" onChange={this.validateCredentials} name="email" />
+                        <div className="form-group">
+                            <label>Email address</label>
+                            <input type="email" className="form-control" placeholder="Enter email" onChange={this.validateCredentials} name="email" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password" className="form-control" placeholder="Enter password" onChange={this.validateCredentials} name="password" />
+                        </div>
+
+                        <div className="form-group">
+                            <div className="custom-control custom-checkbox">
+                                <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                                <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                            </div>
+                        </div>
+
+                        <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                        <p className="forgot-password text-right">
+                            Forgot <a href="#">password?</a>
+                        </p>
+                    </form>
                 </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" onChange={this.validateCredentials} name="password" />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form>
+            </div>
         );
     }
 }
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -43,35 +43,39 @@ export default class SignUp extends Component {
 
     render() {
         return (
-            <form onSubmit={this.submitHandler}>
-                <h3>Sign Up</h3>
+            <div className="auth-wrapper">
+                <div className="auth-inner">
+                    <form onSubmit={this.submitHandler}>
+                        <h3>Sign Up</h3>
 
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" onChange={this.formChange} name="firstName" />
+                        <div className="form-group">
+                            <label>First name</label>
+                            <input type="text" className="form-control" placeholder="First name" onChange={this.formChange} name="firstName" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Last name</label>
+                            <input type="text" className="form-control" placeholder="Last name" onChange={this.formChange} name="lastName" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Email address</label>
+                            <input type="email" className="form-control" placeholder="Enter email" onChange={this.formChange} name="email" />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password" className="form-control" placeholder="Enter password" onChange={this.formChange} name="password" />
+                        </div>
+
+                        <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                        <p className="forgot-password text-right">
+                            Already registered <a href="/">sign in?</a>
+                        </p>
+                        <p className="errorMessage">Error message</p>
+                    </form>
                 </div>
-
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" onChange={this.formChange} name="lastName" />
-                </div>
-
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" onChange={this.formChange} name="email" />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" onChange={this.formChange} name="password" />
-                </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                <p className="forgot-password text-right">
-                    Already registered <a href="/">sign in?</a>
-                </p>
-                <p className="errorMessage">Error message</p>
-            </form>
+            </div>
         );
     }
 }
